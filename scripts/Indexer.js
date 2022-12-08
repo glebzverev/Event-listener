23804eb62170ea821ecc6a3c797a19bbe888d4f5
Gleb Zverev <glebzverev2001@gmail.com>
2022-12-09 02:12:04 +0300

    Fix reconnect and Safety module

--- scripts/Indexer.js
+++ scripts/Indexer.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
-const axios = require('axios')
-const { TOKEN } = process.env
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
 const pairABI = require("../abi/PoolV3ABI.json");
 const {addMint, addSwap, addBurn} = require("./db/db");
 
-const chatId = 968534390
 const ETH_USDT = "0x11b815efB8f581194ae79006d24E0d814B7697F6"
 const ETH_USDC = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
 
@@ -23,12 +19,7 @@ function events (contract, pair) {
             tickevent: tickevent, 
             blockNumber: event.blockNumber
         }
-        // await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        //     chat_id: chatId,
-        //     text: JSON.stringify(info, null, 10)
-        // })
-        await addSwap(info)
-        console.log(event.id)
+        await addSwap(info).then(()=>{console.log("Swap")})
     })
 
     contract.on("Burn", async (  owner, tickLower, tickUpper, amount, amount0, amount1, event) =>{
@@ -43,10 +34,6 @@ function events (contract, pair) {
             amount1: amount1.toString(),  
             blockNumber: event.blockNumber
         }
-        // await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        //     chat_id: chatId,
-        //     text: JSON.stringify(info, null, 9)
-        // })
         await addBurn(info)
     })
 
@@ -63,10 +50,6 @@ function events (contract, pair) {
             amount1: amount1.toString(), 
             blockNumber: event.blockNumber
         }
-        // await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        //     chat_id: chatId,
-        //     text: JSON.stringify(info, null, 10)
-        // })
         await addMint(info)
     })
 }
@@ -77,5 +60,5 @@ function Indexer(provider) {
     const contract2 = new ethers.Contract(ETH_USDC, pairABI, provider);
     events(contract2, "ETH_USDC");
 }
-
+exports.events = events;
 exports.Indexer = Indexer;
--- scripts/Listener.js
+++ scripts/Listener.js
@@ -1,29 +1,12 @@
 require('dotenv').config()
 const ethers = require("ethers")
-const axios = require('axios')
-
-let ClusterMessages = require('cluster-messages');
-let messages = new ClusterMessages();
-
-const { TOKEN, SERVER_URL } = process.env
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
-const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URL + URI
 const EXPECTED_PONG_BACK = 15000
 const KEEP_ALIVE_CHECK_INTERVAL = 7500
 
 const {Indexer} = require("./Indexer.js");
 
-const init_tg = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-    console.log(res.data)
-}
 
 async function Listener(){
-    // await init_tg()
-    
-    // const provider = new ethers.providers.WebSocketProvider(`https://api.etherscan.io/api/apikey=${process.env.API_KEY_ETHERSCAN}`)
-    //const provider = new ethers.providers.WebSocketProvider(`wss://mainnet.infura.io/ws/v3/${process.env.INFURA_SOCKET}`)
     const provider = new ethers.providers.WebSocketProvider(`wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_SOCKET}`)
     const logger = new ethers.utils.Logger();
     
@@ -35,43 +18,50 @@ async function Listener(){
           provider._websocket.ping()
           provider.getBlockNumber().then((res,err)=>{
 		  if (err){
-			  process.send({error: `${err}`})
+			  console.log({error: `${err}`})
 		  }
 		  else if (res){
-			  process.send({blockNumber: res})
+			  console.log({blockNumber: res})
 		  }
 	  })
       pingTimeout = setTimeout(() => {
         provider._websocket.terminate()
       }, EXPECTED_PONG_BACK)
     }, KEEP_ALIVE_CHECK_INTERVAL)
-    
-	  process.send({status: "provider is on"})
-        Indexer(provider);
-      })
-
-    provider._websocket.on('close', () => {
-        logger.warn('The websocket connection was closed')
-        clearInterval(keepAliveInterval)
-        clearTimeout(pingTimeout)
-        Listener()
+	  console.log({status: "provider is on", })
       })
 
     provider._websocket.on('pong', () => {
-        logger.debug('Received pong, so connection is alive, clearing the timeout')
-        clearInterval(pingTimeout)
-        // console.log("pong")
-      })    
+      logger.debug('Received pong, so connection is alive, clearing the timeout')
+      clearInterval(pingTimeout)
+    })    
 
+    provider._websocket.on('close', (err) => {
+        logger.warn('The websocket connection was CLOSED')
+        clearInterval(keepAliveInterval)
+        clearTimeout(pingTimeout)
+        console.error(`${(new Date).toUTCString()} uncaught exception: ${err.message}`);
+        console.error(err.stack);
+        process.exit(1);
+      })
+
+    provider._websocket.on('error', (err) => {
+        logger.warn('The websocket connection ERROR')
+        clearInterval(keepAliveInterval)
+        clearTimeout(pingTimeout)
+        console.error(`${(new Date).toUTCString()} uncaught exception: ${err.message}`);
+        console.error(err.stack);
+        process.exit(1);
+      })
 }
 
 // async function main(){
   // Listener();
 // }
 
-// main().catch((error) => {
-    // console.error(error);
-    // process.exitCode = 1;
-  // });
+Listener().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
 
 exports.Listener = Listener;
